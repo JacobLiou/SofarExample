31084839a9c014cbc3efa31b1954781cbe3332f3
liumenghui <liumenghui@sofarsolar.com>
2024-01-03 17:30:51 +0800

    每天增加一个小demo 掌握nuget核心库 轮子用起来

---
+++ .NET/HumanizorDemo/Program.cs
@@ -0,0 +1,9 @@
+﻿using Humanizer;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("PascalCaseInputStringIsTurnedIntoSentence".Humanize());
+
+var yesterday = DateTime.UtcNow.AddHours(-30).Humanize();
+Console.WriteLine(yesterday);
+Console.WriteLine(DateTimeOffset.UtcNow.AddHours(1).Humanize());
