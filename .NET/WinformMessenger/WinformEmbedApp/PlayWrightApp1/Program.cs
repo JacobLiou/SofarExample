cb58e07d0bf89e76e59da88f4a8628f9ba152d56
liumenghui <liumenghui@sofarsolar.com>
2024-03-12 17:03:38 +0800

    PlayWright vs Selenium

---
+++ .NET/WinformMessenger/WinformEmbedApp/PlayWrightApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Playwright;
+
+Console.WriteLine("Hello, World!");
+
+var playWright = await Playwright.CreateAsync();
+var browser = await playWright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = true});
+
+var page = await browser.NewPageAsync();
+
+await page.GotoAsync("Https://www.baidu.com");
