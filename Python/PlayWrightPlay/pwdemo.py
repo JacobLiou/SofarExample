5d92fa82b533db0d109c1a6f67d6c7eec9184c5a
liumenghui <liumenghui@sofarsolar.com>
2024-05-08 17:59:21 +0800

    浏览器执行端到端测试 -- 使用Selenium或者playwright

---
+++ Python/PlayWrightPlay/pwdemo.py
@@ -0,0 +1,27 @@
+from playwright.sync_api import Playwright, sync_playwright, expect
+
+def run(playwright : Playwright) -> None :
+    def mouse_operate():
+        page.mouse.move(350, 200)
+        page.mouse.down()
+        page.mouse.move(350, 300)
+        page.mouse.move(450, 200)
+        page.mouse.move(350, 200)
+        page.mouse.up()
+
+
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+
+    page.goto("https://draw.yunser.com/")
+    page.wait_for_timeout(1000)
+    mouse_operate()
+    page.wait_for_timeout(1000)
+    context.close()
+    browser.close()
+
+
+with sync_playwright() as playwright :
+    run(playwright)
+
