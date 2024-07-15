d410a573267392990bce515f5b1315f1f8b45668
liumenghui <liumenghui@sofarsolar.com>
2024-07-15 17:24:46 +0800

    拖拽文件打开的Demo

---
+++ .NET/WinFormsApp1/DemoLib/WpfApp1/App.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            //阻塞附加调试
+            MessageBox.Show("sdfdsfds");
+
+            // 检查是否有命令行参数（拖放的文件）
+            if (e.Args.Length > 0)
+            {
+                // 获取拖放的文件路径
+                string[] filePaths = e.Args;
+
+                // 打开主窗口并传递文件路径
+                MainWindow mainWindow = new MainWindow(filePaths);
+                mainWindow.Show();
+            }
+            else
+            {
+                // 正常启动，没有拖放文件
+                MainWindow mainWindow = new MainWindow();
+                mainWindow.Show();
+            }
+        }
+    }
+
+}
---
+++ .NET/WinFormsApp1/DemoLib/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows;
+using System.Windows.Documents;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow(string[] filePaths = null)
+        {
+            InitializeComponent();
+
+            if (filePaths != null && filePaths.Length > 0)
+            {
+                OpenFiles(filePaths);
+            }
+        }
+
+        private void OpenFiles(string[] filePaths)
+        {
+            rich.Document.Blocks.Clear();
+            foreach (string filePath in filePaths)
+            {
+                //// 在这里实现打开文件的逻辑
+                //MessageBox.Show($"Opening file: {filePath}");
+                try
+                {
+                    string text = System.IO.File.ReadAllText(filePath);
+
+                    rich.Document.Blocks.Add(new Paragraph(new Run(text)));
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error loading file: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+
+            }
+        }
+
+    }
+}
